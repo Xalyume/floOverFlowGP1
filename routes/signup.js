b8c02c3449b31c4a16c3416482eabdef1a1db617
William <williamhjang@gmail.com>
2021-08-23 17:48:23 -0400

    login and signup pug files

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
+const { User } = require('../db/models')
 
-router.get('/', (req, res) => {
-    res.send("This is our sign-up page.")
-})
+
+router.get('/', asyncHandler(async (req, res) => {
+    const user = await User.build();
+    res.render("signup", { user, title: "Sign Up" })
+}));
 
 
 module.exports = router;
