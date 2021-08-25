9b07f9ac539bbd5940763df70d69aee3dceabc65
mqu <meitongdepaul@gmail.com>
2021-08-25 09:05:37 -0700

    fix defaultValue of Date in migration file

--- db/migrations/20210824161759-create-answer.js
+++ db/migrations/20210824161759-create-answer.js
@@ -24,12 +24,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        default: new Date()
+        defaultValue: new Date()
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        default: new Date()
+        defaultValue: new Date()
       }
     });
   },
