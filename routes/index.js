b4c8d314b8e45e4ef986f6fbe61c28eb9ee84f43
William <williamhjang@gmail.com>
2021-08-25 11:51:53 -0400

    fixed bug with logged in state for homepage

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
+const { Question, User } = require('../db/models');
 var router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET home page. */
 router.get('/', asyncHandler(async(req, res, next) => {
   if (req.session.auth) {
-    // console.log(req.session.auth.userId);
-    res.render('userhome', { title: 'Welcome Back!' })
+    userId = req.session.auth.userId;
+    const questions = await Question.findAll({
+      where: {
+        userId
+      }
+    })
+    res.render('userhome', { questions, title: 'Welcome Back!' })
   } else {
     res.render('index', { title: 'Welcome to flo Overflow!' });
   }
